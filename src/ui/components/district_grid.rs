51e87ef5bf4356f4a11e86df523e390a8cc1bdba
Jordan Conway <jordan_conway@outlook.com>
2026-07-19 19:31:25 +0100

    Make district display in grid

--- src/ui/components/district_grid.rs
+++ src/ui/components/district_grid.rs
@@ -4,42 +4,47 @@ use crate::{
         DistrictItemDisplay, DistrictItemType, district_item_display,
     },
 };
-use iced::widget::Row;
+use iced::widget::{Column, Row};
 
-pub fn district_grid<'a, Message: 'a>(district: &District) -> Row<'a, Message> {
-    let mut interface = Row::new();
+pub fn district_grid<'a, Message: 'a>(district: &District) -> Column<'a, Message> {
+    let mut grid = Column::new();
 
-    for &color in Color::VARIANTS.iter() {
-        let item = district.get_item(color);
-        match item {
-            Some(district_item) => match district_item {
-                DistrictItem::District(_) => {
+    for i in 0..4 {
+        let mut row = Row::new();
+        for j in 0..4 {
+            let color = Color::VARIANTS[(i * 4) + j];
+            let item = district.get_item(color);
+            match item {
+                Some(district_item) => match district_item {
+                    DistrictItem::District(_) => {
+                        let display = DistrictItemDisplay {
+                            name: "District".to_string(),
+                            color: color,
+                            item_type: DistrictItemType::District,
+                        };
+                        row = row.push(district_item_display(display));
+                    }
+                    DistrictItem::Station(station) => {
+                        let display = DistrictItemDisplay {
+                            name: station.name.to_string(),
+                            color: color,
+                            item_type: DistrictItemType::Station,
+                        };
+                        row = row.push(district_item_display(display));
+                    }
+                },
+                None => {
                     let display = DistrictItemDisplay {
-                        name: "District".to_string(),
+                        name: "None".to_string(),
                         color: color,
-                        item_type: DistrictItemType::District,
+                        item_type: DistrictItemType::None,
                     };
-                    interface = interface.push(district_item_display(display));
+                    row = row.push(district_item_display(display));
                 }
-                DistrictItem::Station(station) => {
-                    let display = DistrictItemDisplay {
-                        name: station.name.to_string(),
-                        color: color,
-                        item_type: DistrictItemType::Station,
-                    };
-                    interface = interface.push(district_item_display(display));
-                }
-            },
-            None => {
-                let display = DistrictItemDisplay {
-                    name: "None".to_string(),
-                    color: color,
-                    item_type: DistrictItemType::None,
-                };
-                interface = interface.push(district_item_display(display));
             }
         }
+        grid = grid.push(row);
     }
 
-    interface
+    grid
 }
